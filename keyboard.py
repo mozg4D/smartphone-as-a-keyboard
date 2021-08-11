3c30bb264bd55c107fcc835ef4609e3cc51ffadb
Artem Rodichkin <48385508+mozg4D@users.noreply.github.com>
2021-08-12 01:23:50 +0300

    Add files via upload

--- keyboard.py
+++ keyboard.py
@@ -9,27 +9,11 @@ import pyqrcode
 import os.path
 
 hllDll = ctypes.WinDLL("User32.dll")
-user32 = ctypes.WinDLL('user32', use_last_error=True)
-wintypes.ULONG_PTR = wintypes.WPARAM
-class MOUSEINPUT(ctypes.Structure): _fields_ = (("dx", wintypes.LONG),("dy", wintypes.LONG),("mouseData", wintypes.DWORD),("dwFlags", wintypes.DWORD),("time", wintypes.DWORD),("dwExtraInfo", wintypes.ULONG_PTR))
-class KEYBDINPUT(ctypes.Structure):
-    _fields_ = (("wVk", wintypes.WORD),("wScan", wintypes.WORD),("dwFlags", wintypes.DWORD),("time", wintypes.DWORD),("dwExtraInfo", wintypes.ULONG_PTR))
-    def __init__(self, *args, **kwds):
-        super(KEYBDINPUT, self).__init__(*args, **kwds)
-        self.wScan = user32.MapVirtualKeyExW(self.wVk, 0, 0)
-class HARDWAREINPUT(ctypes.Structure): _fields_ = (("uMsg", wintypes.DWORD),("wParamL", wintypes.WORD),("wParamH", wintypes.WORD))
-class INPUT(ctypes.Structure):
-    class _INPUT(ctypes.Union): _fields_ = (("ki", KEYBDINPUT),("mi", MOUSEINPUT),("hi", HARDWAREINPUT))
-    _anonymous_ = ("_input",)
-    _fields_ = (("type", wintypes.DWORD),("_input", _INPUT))
-LPINPUT = ctypes.POINTER(INPUT)
-user32.SendInput.argtypes = (wintypes.UINT, LPINPUT, ctypes.c_int)
-def Press(hexKeyCode):
-    x = INPUT(type=1, ki=KEYBDINPUT(wVk=hexKeyCode))
-    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
-def Release(hexKeyCode):
-    x = INPUT(type=1, ki=KEYBDINPUT(wVk=hexKeyCode, dwFlags=0x0002))
-    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+user32 = ctypes.WinDLL('user32')
+class KEYBDINPUT(ctypes.Structure): _fields_ = (("wVk", wintypes.WORD),("dwFlags", wintypes.DWORD),("dwExtraInfo", wintypes.WPARAM))
+class INPUT(ctypes.Structure): _fields_ = (("type", wintypes.DWORD),("ki", KEYBDINPUT),("padding", ctypes.c_ubyte * 8))
+def Press(key):   user32.SendInput(1, ctypes.byref(INPUT(type=1, ki=KEYBDINPUT(wVk=key))), 40)
+def Release(key): user32.SendInput(1, ctypes.byref(INPUT(type=1, ki=KEYBDINPUT(wVk=key, dwFlags=2))), 40)
 
 if(hllDll.GetKeyState(0x90)):
     Press(0x90)
@@ -66,6 +50,7 @@ def repeat_btn():
     global btn_code, cancel_thread, t
     t.cancel()
     if(not cancel_thread) :
+        Release( int( '0x' + btn_code[1:3], 16 ) )
         Press(int( '0x' + btn_code[1:3], 16 ) )
         t=threading.Timer(.04, repeat_btn)
         t.start()
